8bd560d3daffda5b4664fce18f5e59aab1318448
yumikoazu <yumikoazu@163.com>
2015-07-28 22:42:45 +0800

    second commit

--- app/src/main/java/com/yumikoazu/multitaskdownloader/listener/DownloadThreadListener.java
+++
@@ -1,8 +0,0 @@
-package com.yumikoazu.multitaskdownloader.listener;
-
-/**
- * Created by joker on 2015/7/26.
- */
-public interface DownloadThreadListener {
-    void onThreadProgress(int progress);
-}
